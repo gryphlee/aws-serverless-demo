d25059f4e43f080e25a8e99b9d3ca78e01d63530
gryphlee <leenatividad5@gmail.com>
2025-12-05 16:26:42 +0800

    Add secrets and trigger deploy

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'headers': {'Content-Type': 'application/json'},
         'body': json.dumps({
-            "message": "I am now a CI/CD Deployed AI! 100x Engineer Status Unlocked",
+            "message": "Hello from GitHub Actions! I am fully automated now",
             "status": "Online",
             "powered_by": "AWS Lambda + Terraform"
         })
